c2deeef350b79113e7efa1eb4466d45a6192980e
Trevor Strieber <trevor@strieber.org>
2014-01-17 16:53:56 -0500

    Ran annotate.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  start      :datetime
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Event < ActiveRecord::Base
 end
