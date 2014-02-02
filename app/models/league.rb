5f8669a4598ac6bf2b3b74c09f8128af24947e69
Trevor Strieber <trevor@strieber.org>
2014-02-02 00:06:46 -0500

    Adding methods to get League teams.

--- app/models/league.rb
+++ app/models/league.rb
@@ -10,4 +10,12 @@
 
 class League < ActiveRecord::Base
   has_many :teams
+
+  def self.NL
+    where(name: 'National League').first
+  end
+
+  def self.AL
+    where(name: 'American League').first
+  end
 end
